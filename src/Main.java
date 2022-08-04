df3a44ca78f2917d965236d24b9e593cc6f79a59
Staluks <janchukssss@inbox.lv>
2022-08-04 14:37:05 +0300

    commit push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DBlogic db = new DBlogic();
+//        db.insert();
+        db.delete();
     }
 }
